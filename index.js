1d120ec57d842f29d36950deb2a50c50cc35491c
ivor <ivord18@gmail.com>
2026-04-26 17:55:15 -0500

    actualizacion: endpoint citas

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const db = mysql.createConnection({
     password: '',
     database: 'barberia_db'
 });
-
+// servicios
 db.connect(err => {
     if (err) throw err;
     console.log("✅ Conectado a la Base de Datos");
@@ -21,5 +21,19 @@ app.get('/servicios', (req, res) => {
         res.json(results);
     });
 });
-
+// citas
+app.post('/citas', (req, res) => {
+    const { nombre_cliente, servicio_id, fecha, hora } = req.body;
+    const sql = 'INSERT INTO citas (nombre_cliente, servicio_id, fecha, hora) VALUES (?, ?, ?, ?)';
+    
+    db.query(sql, [nombre_cliente, servicio_id, fecha, hora], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.status(201).json({ 
+            mensaje: '✅ Cita agendada con éxito en la Barbería Wason', 
+            id_cita: result.insertId 
+        });
+    });
+});
 app.listen(3000, () => console.log(" Servidor en puerto 3000"));
